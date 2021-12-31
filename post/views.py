547fbb69dafb371debb2152d56c87167bd291609
guang <511143221@qq.com>
2021-12-31 16:12:34 +0800

    fix the asview error2

--- post/views.py
+++ post/views.py
@@ -2,11 +2,12 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.http import require_GET
 from django.views import View
 
+
 class PostView(View):
     pass
+
 
 def getpost(request: HttpRequest, id):
     print(id)
 
     return JsonResponse({}, status=201)
-
