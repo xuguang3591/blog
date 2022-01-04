4f74d2ebec456fee966ff77738c12a14f7954ebd
guang <511143221@qq.com>
2022-01-04 12:04:51 +0800

    page compeleted

--- post/views.py
+++ post/views.py
@@ -1,3 +1,4 @@
+import math
 import simplejson
 import datetime
 from django.http import HttpRequest, HttpResponse, JsonResponse
@@ -9,29 +10,39 @@ from user.models import User
 from utils import jsonify
 
 
+def validate(d: dict, name: str, type_func, default, validate_func):
+    try:
+        ret = type_func(d.get(name, default))
+        ret = validate_func(ret, default)
+    except:
+        ret = default
+    return ret
+
+
 class PostView(View):
     def get(self, request: HttpRequest):
         print('get ~~~~~~~~~~~~~')
-        try:
-            page = int(request.GET.get('page', 1))
-            page = page if page > 0 else 1
-        except:
-            page = 1
-        try:
-            size = int(request.GET.get('size', 20))
-            size = size if size > 0 and size < 101 else 20
-        except:
-            size = 20
+        page = validate(request.GET, 'page', int, 1, lambda x, y: x if x > 0 else y)
+        size = validate(request.GET, 'size', int, 20, lambda x, y: x if x > 0 and x < 101 else y)
         try:
             start = (page - 1) * size
-            posts = Post.objects.order_by('pk')[start:start + size]
+            posts = Post.objects.order_by('pk')
+            print(posts.query)
+            total = posts.count()
+            posts = posts[start:start + size]
             print(posts.query)
 
             return JsonResponse({
                 'posts': [
                     jsonify(post, allow=['id', 'title'])
                     for post in posts
-                ]
+                ],
+                'pagination': {
+                    'page': page,
+                    'size': size,
+                    'total': total,
+                    'pages': math.ceil(total / size)
+                }
             })
         except Exception as e:
             print(e)
